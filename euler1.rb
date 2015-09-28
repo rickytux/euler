be584110416360c04bb03508ac3fb2990002a81c
Rick D <rickytux22@gmail.com>
2015-09-28 16:26:12 -0500

    Update euler1.rb

--- euler1.rb
+++ euler1.rb
@@ -1,12 +1,1 @@
-x =1
-multiples = []
-until x >=1000
-	if x%3==0 || x %5 == 0
-		multiples << x
-	end
-	x+=1
-end
-
-sum = multiples.inject(:+)
-print multiples
-print sum
+print Array(1..x).find_all { |item| item%3 == 0 || item%5 == 0 }.inject(:+)
